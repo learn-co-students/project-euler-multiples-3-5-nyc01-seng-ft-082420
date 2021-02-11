5caeb66cf37e474051325fbf4e415b2378d4e5ec
soroosh-gb <soroosh.gb@gmail.com>
2021-02-10 19:09:42 -0500

    js

--- multiples.js
+++ multiples.js
@@ -1,15 +1,7 @@
 function sumUp(num){
-    let currentNum = num;
-    let multiplies = [];
     let sum = 0;
-    while(currentNum > 0){
-      if(currentNum %3 === 0 || currentNum %5 === 0){
-          multiplies.push(currentNum);
-          currentNum = currentNum - 1;
-      } 
-    } 
-      for(let i = 0; i < multiplies.length; i++){
-          sum +=multiplies[i]
-      }
+    for(let i = 0; i < num; i++){
+        if(i %3 === 0 || i %5 === 0) sum = sum + i
+    }
     return sum;
 }
